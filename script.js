825fd7f65008e6c2ce586aecdc7590ad05e7ac56
Malik Sadiki-Torres <maliktorres98@gmail.com>
2024-10-24 18:55:25 -0400

    added notes regarding this keyword

--- script.js
+++ script.js
@@ -1,79 +1,119 @@
 'use strict';
 
-// parent scope
-function calcAge(birthYear) {
-  const age = 2024 - birthYear
+// // parent scope
+// function calcAge(birthYear) {
+//   const age = 2024 - birthYear
 
-  function printAge() {
-    let output = `${firstName} You are ${age}. born in ${birthYear}`
-    console.log(output);
+//   function printAge() {
+//     let output = `${firstName} You are ${age}. born in ${birthYear}`
+//     console.log(output);
 
-    // Block scope
-    if (birthYear >= 1981 && birthYear >= 1996) {
-      // creating New variable with same name as outer scope's variable 
-      const firstName = `Bob`
+//     // Block scope
+//     if (birthYear >= 1981 && birthYear >= 1996) {
+//       // creating New variable with same name as outer scope's variable 
+//       const firstName = `Bob`
 
-      // reassigning outer scope's variable 
-      output = 'NEW OUTPUT!';
-      const str = `0h, and you're not a millenial, ${firstName}`;
-      console.log(str);
+//       // reassigning outer scope's variable 
+//       output = 'NEW OUTPUT!';
+//       const str = `0h, and you're not a millenial, ${firstName}`;
+//       console.log(str);
 
-      function add(a, b) {
-        return a + b;
-      }
+//       function add(a, b) {
+//         return a + b;
+//       }
 
-      console.log(output);
+//       console.log(output);
 
-    }
+//     }
+//   }
+//   printAge();
+
+//   return age;
+// }
+// // Global Scope
+// const firstName = 'Malik';
+// calcAge(1998);
+
+// // Variable Hoisting
+// console.log(me);
+// // console.log(job);
+// // console.log(year);
+
+// var me = 'Malik';
+// let job = 'digital marketer';
+// const year = 1998;
+
+// // Functions
+// console.log(addDecl(2,3));
+// // console.log(addExpr(2,3));
+// console.log(addArrow);
+// // console.log(addArrow(2,3));
+
+
+// function addDecl(a,b) {
+//   return a + b;
+// }
+
+// const addExpr = function(a,b) {
+//   return a + b
+// }
+
+// var addArrow = (a ,b) => a + b; 
+
+// // Example
+// console.log(undefined);
+// if(!numProducts) deleteShoppingCart();
+
+// var numProducts = 10;
+
+// function deleteShoppingCart() {
+//   console.log('All products deleted!');
+// } 
+
+// var x = 1;
+// let y = 2;
+// const z = 3; 
+
+// console.log(x === window.x);
+// console.log(y === window.y);
+// console.log(z === window.z); 
+
+
+
+// THIS KEYWORD
+// console.log(this); 
+
+const calcAge = function(birthYear) {
+  console.log(2024 - birthYear);
+  // console.log(this);
+};
+
+calcAge(1998); 
+
+
+const calcAgeArrow = birthYear => {
+  console.log(2024 - birthYear);
+  // console.log(this);
+};
+
+calcAgeArrow(1998); 
+
+const malik = {
+  year: 1998,
+  calcAge: function() {
+    console.log(this);
+    console.log(2024 - this.year);
   }
-  printAge();
+};
 
-  return age;
-}
-// Global Scope
-const firstName = 'Malik';
-calcAge(1998);
+malik.calcAge(); 
 
-// Variable Hoisting
-console.log(me);
-// console.log(job);
-// console.log(year);
+const carmen = {
+  year: 1998,
+};
+// method borrowing
+carmen.calcAge = malik.calcAge; 
+carmen.calcAge();
 
-var me = 'Malik';
-let job = 'digital marketer';
-const year = 1998;
-
-// Functions
-console.log(addDecl(2,3));
-// console.log(addExpr(2,3));
-console.log(addArrow);
-// console.log(addArrow(2,3));
-
-
-function addDecl(a,b) {
-  return a + b;
-}
-
-const addExpr = function(a,b) {
-  return a + b
-}
-
-var addArrow = (a ,b) => a + b; 
-
-// Example
-console.log(undefined);
-if(!numProducts) deleteShoppingCart();
-
-var numProducts = 10;
-
-function deleteShoppingCart() {
-  console.log('All products deleted!');
-} 
-
-var x = 1;
-let y = 2;
-const z = 3; 
-
-console.log(x === window.x);
-console.log(y === window.y);
-console.log(z === window.z);
+const f = malik.calcAge; 
+f();
